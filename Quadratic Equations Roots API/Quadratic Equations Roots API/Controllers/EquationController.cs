4801c2208bdaf5c8132d2ee316d00627408e0d76
Artem Sergeev <aesergnsk@gmail.com>
2023-02-18 09:49:04 +0200

    different fixes

--- Quadratic Equations Roots API/Quadratic Equations Roots API/Controllers/EquationController.cs
+++ Quadratic Equations Roots API/Quadratic Equations Roots API/Controllers/EquationController.cs
@@ -4,24 +4,17 @@ using Quadratic_Equations_Roots_API.Logic;
 namespace Quadratic_Equations_Roots_API.Controllers;
 
 [ApiController]
-[Route("Equation")]
+[Route("equation")]
 public class EquationController : Controller
 {
-    [HttpPost("Post")]
-    public double[] PostSet([FromBody] Equation equantion)
+    [HttpPost("post")]
+    public double[] PostSet([FromBody] Equation equation)
     {
-        return equantion.SolveRoots();
-    }
-    
-    [HttpGet("Get")]
-    public double[] Set(double a, double b, double c)
-    {
-        var equation = new Equation(a, b, c);
         return equation.SolveRoots();
     }
     
-    [HttpGet("{a}/{b}/{c}")] 
-    public double[] Set2(double a, double b, double c)
+    [HttpGet("get")]
+    public double[] Set(double a, double b, double c)
     {
         var equation = new Equation(a, b, c);
         return equation.SolveRoots();
--- Quadratic Equations Roots API/Tests/UnitTest1.cs
+++ Quadratic Equations Roots API/Tests/UnitTest1.cs
@@ -1,86 +1,97 @@
+using System;
+using System.Collections;
 using Quadratic_Equations_Roots_API.Logic;
 
 namespace Tests;
 
 public class Tests
 {
-    const double  Delta = 0.01;
-    
-    [Test]
-    public void TwoRootsTest1()
+    const double Delta = 0.01;
+
+    [TestCaseSource(nameof(GenerateTestCasesForTwoRootsTest))]
+    public void TwoRootsTest1(int a, int b, int c, double[] expectedRoots)
     {
-        var equation = new Equation(2,5,-3);
-        var ans = new double[2] { -3, 0.5};
-        
-        Assert.That(equation.SolveRoots(), Is.EqualTo(ans));
+        //arrange
+        var equation = new Equation(a, b, c);
+
+        //act
+        var roots = equation.SolveRoots();
+
+        //assert
+        Assert.That(roots[0], Is.EqualTo(expectedRoots[0]).Within(Delta));
+        Assert.That(roots[1], Is.EqualTo(expectedRoots[1]).Within(Delta));
     }
-    
-    [Test]
-    public void TwoRootsTest2()
-    {
-        var equation = new Equation(3,-10,3);
-        var ans = new double[2] { 0.33, 3};
-        
-        Assert.That(equation.SolveRoots()[0], Is.EqualTo(ans[0]).Within(Delta));
-        Assert.That(equation.SolveRoots()[1], Is.EqualTo(ans[1]).Within(Delta));
-    }
-    
+
     [Test]
     public void TwoRootsTest3()
     {
-        var equation = new Equation(4,21,5);
-        var ans = new double[2] { -5, -0.25};
-        
-        Assert.That(equation.SolveRoots()[0], Is.EqualTo(ans[0]).Within(Delta));
-        Assert.That(equation.SolveRoots()[1], Is.EqualTo(ans[1]).Within(Delta));
+        var equation = new Equation(4, 21, 5);
+        var expectedRoots = new double[2] { -5, -0.25 };
+
+        Assert.That(equation.SolveRoots()[0], Is.EqualTo(expectedRoots[0]).Within(Delta));
+        Assert.That(equation.SolveRoots()[1], Is.EqualTo(expectedRoots[1]).Within(Delta));
     }
-    
+
     [Test]
     public void TwoRootsTest4()
     {
-        var equation = new Equation(5,-14,-3);
-        var ans = new double[2] { -0.2, 3};
-        
-        Assert.That(equation.SolveRoots()[0], Is.EqualTo(ans[0]).Within(Delta));
-        Assert.That(equation.SolveRoots()[1], Is.EqualTo(ans[1]).Within(Delta));
+        var equation = new Equation(5, -14, -3);
+        var expectedRoots = new[] { -0.2, 3 };
+        var roots = equation.SolveRoots();
+
+        Assert.That(roots[0], Is.EqualTo(expectedRoots[0]).Within(Delta));
+        Assert.That(roots[1], Is.EqualTo(expectedRoots[1]).Within(Delta));
     }
-    
+
+    public class DoubleComparer : IComparer
+    {
+        private double _eps = 0.0000001;
+
+        public int Compare(object? x, object? y)
+        {
+            var xValue = x is double dx ? dx : 0;
+            var yValue = x is double dy ? dy : 0;
+            if (Math.Abs(xValue - yValue) < _eps)
+            {
+                return 0;
+            }
+
+            return 1;
+        }
+    }
+
     [Test]
     public void OneRootTest1()
     {
-        var equation = new Equation(9,-30,25);
-        var ans = new double[1] {1.66};
-        
-        Assert.That(equation.SolveRoots(), Is.EqualTo(ans).Within(Delta));
+        var equation = new Equation(9, -30, 25);
+        var ans = new double[1] { 1.66 };
+        var solvedRoots = equation.SolveRoots();
+        //
+        // Assert.That(equation.SolveRoots(), Is.EqualTo(ans).Within(Delta));
     }
-    
+
     [Test]
     public void OneRootTest2()
     {
-        var equation = new Equation(1,12,36);
-        var ans = new double[1] {-6};
-        
+        var equation = new Equation(1, 12, 36);
+        var ans = new double[1] { -6 };
+
         Assert.That(equation.SolveRoots(), Is.EqualTo(ans).Within(Delta));
     }
-    
+
     [Test]
     public void NoRootsTest1()
     {
-        var equation = new Equation(1,12,36);
-        
-        // Эта штука  не работает, он всегда показывает Succes;
-        try
-        {
-            equation.SolveRoots();
-            Assert.Fail();
-        }
-        catch (Exception e)
-        {
-            Assert.Pass();
-        }
-        // Я пытался сделать так, но что то пошло не так :(
-        // Assert.Throws<System.Exception>(
-        //     equation.SolveRoots() => throw new Exception());
+        var equation = new Equation(1, 12, 40);
+        Assert.Throws<Exception>(() => equation.SolveRoots());
     }
-    
+
+    private static object[][] GenerateTestCasesForTwoRootsTest()
+    {
+        return new[]
+        {
+            new object[] { 2, 5, -3, new[] { -3, 0.5 } },
+            new object[] { 3, -10, 3, new[] { 0.33, 3 } },
+        };
+    }
 }
